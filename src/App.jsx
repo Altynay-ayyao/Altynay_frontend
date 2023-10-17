682d8257cae12a9abab1dd6066bb54e82b69d765
Altynay <altyn@Altun_kotikone.(none)>
2023-10-17 12:20:25 +0300

    pages added

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { About,Cart,Checkout,Error,HomeLayout,Landing,Login,Orders,Products,Register,SingleProduct } from "./pages";
+
+const App = () => {
+  return (
+<div>
+  <About/>
+  <Cart/>
+  <Checkout/>
+  <Error/>
+  <HomeLayout/>
+  <Landing/>
+  <Login/>
+  <Orders/>
+  <Products/>
+  <Register/>
+  <SingleProduct/>
+</div>
+  )
+}
+
+export default App;
