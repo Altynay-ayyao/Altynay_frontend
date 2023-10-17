7f1ba21a25a89bd4f303844ea57e8151f4cda4dc
Altynay <altyn@Altun_kotikone.(none)>
2023-10-17 16:53:17 +0300

    SubmitBtn setup

--- src/components/SubmitBtn.jsx
+++ src/components/SubmitBtn.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { useNavigation } from 'react-router-dom'
 
 const SubmitBtn = ({text}) => {
+const navigation = useNavigation()
+const isSubmitting = navigation.state === 'submitting'
+
+
   return (
-    <button type='submit' className='btn btn-neutral' >{text}</button>
+    <button type='submit' className='btn btn-neutral btn-block' disabled={isSubmitting}>
+        {
+            isSubmitting? (<>
+                <span className='loading loading-dots'></span>
+                submitting...
+                </>):(text || 'submit')
+        }
+    </button>
   )
 }
 
 export default SubmitBtn
+
+//if the navigation state is equal to submitting
