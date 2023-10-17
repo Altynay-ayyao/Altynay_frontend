6883eee02716a9e194029f1a77ffa1371ba1ba84
Altynay <altyn@Altun_kotikone.(none)>
2023-10-17 19:25:43 +0300

    NavLinks are displaying the NavLink

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import {BsCart3, BsMoonFill, BsSunFill} from 'react-icons/bs';
 import {FaBarsStaggered} from 'react-icons/fa6';
 import { NavLink } from 'react-router-dom';
 import logo from '../assets/logo.svg'
+import NavLinks from './NavLinks';
 
 const Navbar = () => {
   return (
@@ -18,19 +19,22 @@ const Navbar = () => {
                         <FaBarsStaggered className='h-6 w-6'/>
                     </label>
                     <ul tabIndex={0} className='menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-200'>
-                        nav links
+                        <NavLinks/>
                     </ul>
                 </div>
             </div>
             <div className="navbar-center hidden lg:flex">
-                <ul className='menu menu-horizontal'>nav links</ul>
+                <ul className='menu menu-horizontal'><NavLinks/></ul>
             </div>
             <div className="navbar-end">
                 {/* THEME SETUP */}
                 {/* CART LINK */}
                 <NavLink to='/cart' className='btn btn-ghost btn-circle btn-md ml-4'>
                     <div className="indicator">
-                        <BsCart3 className='h-6 w-6'/>
+                         <div className="indicator">
+                            <BsCart3 className='h-6 w-6'/>
+                            <span className='badge badge-sm badge-primary indicator-item'>8</span>
+                         </div>
                         <span className="badge badge-sm badge-primary indicator-item">8</span>
                     </div>
                 </NavLink>
