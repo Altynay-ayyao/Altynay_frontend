efa3856c5e46fe0277cf161675db83dc97afc23f
Altynay <altyn@Altun_kotikone.(none)>
2023-10-17 17:05:31 +0300

    Register page setup

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,7 +6,7 @@ const Login = () => {
   return (
     <section className='h-screen grid place-items-center'>
       <Form
-        method='post'
+        method='POST'
         className='card w-96 py-8 px-8 bg-base-100 shadow-log flex flex-col gap-y-4'>
         <h4 className='text-center text-3xl font-bold'>Login</h4>
         <FormInput
@@ -27,7 +27,7 @@ const Login = () => {
         </button>
         <p className='text-center'>
           Not a member yet?
-          <Link to='./register'
+          <Link to='/register'
             className='ml-2 link link-hover link-primary capitalize'>
             Register
           </Link>
