5bc8b372591e304d1f388d076485bee9bad30135
Altynay <altyn@Altun_kotikone.(none)>
2023-10-23 20:25:22 +0300

    cartTotals setup

--- src/components/CartTotals.jsx
+++ src/components/CartTotals.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const CartTotals = () => {
+  const {cartTotal, shipping,tax,orderTotal} = useSelector((state)=>state.cartState)
   return (
-    <div className='text-3xl'>CartTotals</div>
+    <div className='card bg-base-200'>
+      <div className="card-body">
+        {/* SUBTOTAL */}
+        <p className='flex justify-between text-xs border-b border-base-300 pb-2'>
+          <span>Subtotal</span>
+          <span className='font-medium'>{cartTotal}€</span>
+        </p>
+        {/* SHIPPING */}
+        <p className='flex justify-between text-xs border-b border-base-300 pb-2'>
+          <span>Shipping</span>
+          <span className='font-medium'>{shipping}€</span>
+        </p>
+        {/* TAX */}
+        <p className='flex justify-between text-xs border-b border-base-300 pb-2'>
+          <span>Tax</span>
+          <span className='font-medium'>{tax}€</span>
+        </p>
+        {/* ORDETtOTAL */}
+        <p className='flex justify-between text-sm border-b border-base-300 pb-2'>
+          <span>Order Total</span>
+          <span className='font-medium'>{orderTotal+tax+shipping+cartTotal}€</span>
+        </p>
+      </div>
+    </div>
   )
 }
 
