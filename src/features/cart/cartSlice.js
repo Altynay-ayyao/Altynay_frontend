da8c173fddcae22de592ce1b677349a701900250
Altynay <altyn@Altun_kotikone.(none)>
2023-10-23 18:28:44 +0300

    remove items function set

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -10,9 +10,13 @@ const defaultState = {
   orderTotal:0,
 }
 
+const getCartFromLocalStorage = ()=>{
+  return JSON.parse(localStorage.getItem('cart'))||defaultState;
+}
+
 const cartSlice = createSlice({
   name:'cart',
-  initialState:defaultState,
+  initialState:getCartFromLocalStorage(),
   reducers:{
     addItem:(state,action)=>{
      
@@ -24,17 +28,35 @@ const cartSlice = createSlice({
       } else{
         state.cartItems.push(product);
       }
-
+      cartSlice.caseReducers.calculateTotals(state)
       state.numItemsInCart += product.amount
       state.cartTotal+=product.price*product.amount
-      state.tax = 0.2 * state.cartTotal
-      state.orderTotal = state.cartTotal+state.shipping + state.tax;
-      localStorage.setItem('cart',JSON.stringify(state))
       toast.success('Item added to cart')
     },
-    clearCart:(state)=>{},
-    removeItem:(state,action)=>{},
-    editItem:(state,action)=>{},
+
+    clearCart:(state)=>{
+      localStorage.setItem('cart',JSON.stringify(defaultState));
+      return defaultState
+    },
+    
+    removeItem:(state,action)=>{
+      const {cartID}=action.payload;
+      const product = state.cartItems.find((i)=>i.cartID === cartID);
+      state.cartItems = state.cartItems.filter((i)=>i.cartID !== cartID);
+      state.numItemsInCart -= product.amount
+      state.cartTotal-=product.price*product.amount;
+      cartSlice.caseReducers.calculateTotals(state);
+      toast.error('Item removed from cart')
+
+    },
+    editItem:(state,action)=>{
+      
+    },
+    calculateTotals:(state)=>{
+      state.tax = 0.1 * state.cartTotal
+      state.orderTotal = state.cartTotal+state.shipping + state.tax;
+      localStorage.setItem('cart',JSON.stringify(state))
+    }
   }
 })
 
